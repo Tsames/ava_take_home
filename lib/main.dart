9b5fa7ebf076e894941dab61b860eeee8d332d21
Tsames <tsames1@gmail.com>
2025-06-02 14:27:02 -0700

    Began adding riverpod [wip]

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+// part 'main.g.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      ProviderScope(
+          child: const MyApp()
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
