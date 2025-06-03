85281bb2405ffd854967a3e37306ee7d23c51352
Tsames <tsames1@gmail.com>
2025-06-02 18:34:12 -0700

    Added text styles and custom fonts to theme data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:meet_ava_take_home/home_page.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ),
+  );
   runApp(const AvaApp());
 }
 
@@ -18,8 +27,19 @@ class AvaApp extends StatelessWidget {
             onPrimary: const Color(0xFF2A1E39),
             onPrimaryFixed: const Color(0xFF736B7C),
             onPrimaryFixedVariant: const Color(0xFFA448FF),
+            secondary: const Color(0xFF48A388),
+            secondaryContainer: const Color(0xFFA9EACE),
+            tertiaryContainer: const Color(0xFF420085),
+            onTertiaryContainer: const Color(0xFFFFFFFF),
             outline: const Color(0x26000000),
             surface: const Color(0xFFF2F0ED),
+        ),
+        textTheme: const TextTheme(
+          headlineSmall: TextStyle(fontFamily: "AtHauss", fontSize: 16, fontWeight: FontWeight.w600),
+          headlineMedium: TextStyle(fontFamily: "AtHauss", fontSize: 22),
+          bodyMedium: TextStyle(fontFamily: "AtHauss", fontSize: 14),
+          bodySmall: TextStyle(fontFamily: "AtHauss", fontSize: 12),
+          labelLarge: TextStyle(fontFamily: "AtSlamCnd", fontSize: 36, fontWeight: FontWeight.w600),
         )
       ),
     );
